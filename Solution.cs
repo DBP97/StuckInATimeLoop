8b98f36b158fce28b413905ff01489896f423887
DBP97 <89939852+DBP97@users.noreply.github.com>
2022-03-08 02:20:28 -0500

    Update Solution.cs

--- Solution.cs
+++ Solution.cs
@@ -11,7 +11,7 @@ namespace StuckInATimeLoop
             int num = int.Parse(Console.ReadLine());
 
             // Creating for loop that stars at 1 and ends where the input is
-            for (int x = 1; x <= num; x++)
+            for (int x = 1; x <= num && x <= 100; x++)
             {
                 // Asked sentence is created for every loop
                 Console.WriteLine(x + " " + "Abracadabra");
